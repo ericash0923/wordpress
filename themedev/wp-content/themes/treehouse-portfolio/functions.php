e7e84e92fb315b5ce6e4c3c8380d3917068842ae
Eric Ashendorf <ericashendorf@Erics-iMac.local>
2018-07-30 15:56:11 -0400

    add js files to theme folder

--- themedev/wp-content/themes/treehouse-portfolio/functions.php
+++ themedev/wp-content/themes/treehouse-portfolio/functions.php
@@ -10,5 +10,14 @@ function wpt_theme_style() {
 
 add_action( 'wp_enqueue_style', 'wpt_theme_style' );
 
+function wpt_theme_js() {
+
+	wp_enqueue_script( 'modernizer_js', get_template_diredtory_uri() . '/js/modernizer.js', '', '', false );
+	wp_enqueue_script( 'foundation_js', get_template_diredtory_uri() . '/js/foundation.js', array('jquery'), '', true );
+	wp_enqueue_script( 'main_js', get_template_diredtory_uri() . '/js/app.js', array('jquery', 'foundation.js'), '', true );
+}
+
+add_action( 'wp_enqueue_script', 'wpt_theme_js');
+
 
 ?>
---
+++ themedev/wp-content/themes/treehouse-portfolio/js/app.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function( $ ) {
+
+	$(document).foundation();
+	$( ".nav-toggle" ).click(function() {
+  		$(this).toggleClass("open");
+  		$("nav").fadeToggle(100);
+
+  		return false;
+	});
+});
+
+
+
+
+$( ".nav-toggle" ).click(function() {
+  $(this).toggleClass("open");
+  $("nav").fadeToggle(100);
+
+  return false;
+});
