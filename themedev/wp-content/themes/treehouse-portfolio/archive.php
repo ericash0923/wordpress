9ce18697ec9352148e940a4dc2f9a364e7fc0f8c
Eric Ash <eric@gaglani.com>
2018-08-12 16:48:52 -0400

    setup comments on single.php and archive page

--- themedev/wp-content/themes/treehouse-portfolio/archive.php
+++ themedev/wp-content/themes/treehouse-portfolio/archive.php
@@ -7,30 +7,30 @@
       <div class="small-12 medium-7 medium-offset-1 medium-push-4 columns">
         <div class="primary">
 
+          <div class="leader">
+            <h1><?php wp_title(''); ?></h1>
+          </div>
+    
           <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
           
           <article class="post">
-            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-            <ul class="post-meta no-bullet">
-              <li class="author">
-                <span class="wpt-avatar small">
-                  <?php echo get_avatar( get_the_author_meta( 'ID' ), 24); ?>
-                </span>
-                by <?php the_author_posts_link(); ?>
-              </li>
-              <li class="cat">in <?php the_category( ' '); ?></li>
-              <li class="cat">on <?php the_time('F j, Y'); ?></li>
-            </ul>
-
+              <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+              <h2><?php echo strip_tags( get_the_excerpt() ); ?></h2>
+              <ul class="post-meta no-bullet">
+                <li class="author">
+                  <span class="wpt-avatar small">
+                    <?php echo get_avatar( get_the_author_meta( 'ID' ), 24); ?>
+                  </span>
+                  by <?php the_author_posts_link(); ?>
+                </li>
+                <li class="cat">in <?php the_category( ' '); ?></li>
+                <li class="cat">on <?php the_time('F j, Y'); ?></li>
+              </ul>
               <?php if( get_the_post_thumbnail() ) : ?>
                 <div class="img-container">
                   <?php the_post_thumbnail('large'); ?>
                 </div>
               <?php endif; ?>
-
-              <?php the_content(); ?>
-              <?php comments_template(); ?>
-              
             </article>
       
           <?php endwhile; else : ?>
